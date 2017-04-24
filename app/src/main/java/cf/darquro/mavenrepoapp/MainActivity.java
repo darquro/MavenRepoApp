703a666af01f8b44774b94f8ecc65f80626a9201
Yuki Kuroda <yuki.kuroda@supership.jp>
2017-04-24 13:56:43 +0900

    update

--- app/src/main/java/cf/darquro/mavenrepoapp/MainActivity.java
+++ app/src/main/java/cf/darquro/mavenrepoapp/MainActivity.java
@@ -2,6 +2,12 @@ package cf.darquro.mavenrepoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import cf.darquro.mylibrary.Foo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(new Foo().bar());
     }
 }
